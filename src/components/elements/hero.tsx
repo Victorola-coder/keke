03345c6aac1a034f50e866360996a40647af3187
Victorola <victoluolatunji@gmail.com>
2024-09-01 12:28:28 -0700

    Updated Hero component: added flex container, rearranged elements, and added image figure

--- src/components/elements/hero.tsx
+++ src/components/elements/hero.tsx
@@ -1,8 +1,17 @@
 export default function Hero() {
   return (
     <section className="bg-[#212121]">
-      <div>
-        <h3 className="text-[72px] h-full">Ride Smart, Ride Keke</h3>
+      <div className="flex flex-row items-start justify-between">
+        <div>
+          <h3 className="text-[72px] max-w-[581px] leading-[82.79px">
+            Ride Smart, Ride Keke
+          </h3>
+        </div>
+
+        {/* image */}
+        <figure>
+          <img src="/images/hero.svg" alt="hero_image" loading="lazy" />
+        </figure>
       </div>
     </section>
   );
