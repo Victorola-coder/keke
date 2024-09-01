e6b2c574bfeb19003e73970debc8e48f57536700
Victorola <victoluolatunji@gmail.com>
2024-09-01 08:27:28 -0700

    Remove background color from navbar header element.

---
+++ src/components/elements/hero.tsx
@@ -0,0 +1,7 @@
+export default function Hero() {
+  return (
+    <section className="bg-[#212121]">
+      <div></div>
+    </section>
+  );
+}
--- src/components/molecules/navbar.tsx
+++ src/components/molecules/navbar.tsx
@@ -1,3 +1,35 @@
+import { Button } from "../atoms";
+import { Dropdown, Language, Logo } from "../svgs";
+
 export default function Navbar() {
-  return <div>Navbar</div>;
+  return (
+    <header className="bg-[#212121]">
+      <nav className="mx-[90px] py-[20px] flex flex-row items-center justify-between">
+        <figure className="flex flex-row items-center gap-[14px]">
+          <img
+            alt="logo"
+            loading="lazy"
+            draggable="false"
+            src="/images/logo.svg"
+            className="transition-all duration-300 transform  hover:rotate-[720deg] active:rotate-180 cursor-pointer"
+          />
+          <Logo fill="white" />
+        </figure>
+
+        <div className="flex flex-row items-center gap-[20px]">
+          <ul>
+            <li className="whitespace-nowrap text-white text-base leading-[18.4px] capitalize transition-all duration-300 hover:text-white/80">
+              contact us
+            </li>
+          </ul>
+          <h3 className="flex flex-row items-end gap-1 text-white text-base leading-[18.4px] capitalize transition-all duration-300 hover:text-white/80">
+            <Language />
+            English
+            <Dropdown className="cursor-pointer" />
+          </h3>
+          <Button className="bg-[#CB8806]">Download App</Button>
+        </div>
+      </nav>
+    </header>
+  );
 }
